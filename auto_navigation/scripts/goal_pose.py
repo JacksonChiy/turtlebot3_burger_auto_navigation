6fbdad92a6261326064e99926efbd1fea6c13fa9
JacksonChiy <51813740+JacksonChiy@users.noreply.github.com>
2023-11-19 10:15:16 -0500

    Pushed new goal_pose.py

--- auto_navigation/scripts/goal_pose.py
+++ auto_navigation/scripts/goal_pose.py
@@ -3,113 +3,75 @@
 import rospy
 import math
 import actionlib
+from nav_msgs.msg import Odometry
+from geometry_msgs.msg import Twist
 from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
 from tf.transformations import euler_from_quaternion, quaternion_from_euler
 from geometry_msgs.msg import PoseStamped
 
-def active_cb(extra):
-    rospy.loginfo("Goal pose being processed")
-def feedback_cb(feedback):
-    # rospy.loginfo("Current location: "+str(feedback))
-    pass
-def done_cb(status, result):
-    if status == 3:
-        rospy.loginfo("Goal reached")
-    if status == 2 or status == 8:
-        rospy.loginfo("Goal cancelled")
-    if status == 4:
-        rospy.loginfo("Goal aborted")
-
-#Initial Node
 rospy.init_node('goal_pose')
-
-#Get goal position from ArUco marker
-goal_pose = rospy.wait_for_message('/id101/aruco_single/pose', PoseStamped)
+# goal_pose = rospy.wait_for_message('/id101/aruco_single/pose', PoseStamped)
 
 
-def rotate(origin, point, angle):
-    """
-    Rotate a point counterclockwise by a given angle around a given origin.
 
-    The angle should be given in radians.
-
-    source: https://stackoverflow.com/a/34374437
-    """
-    ox, oy = origin
-    px, py = point
-
-    qx = ox + math.cos(angle) * (px - ox) - math.sin(angle) * (py - oy)
-    qy = oy + math.sin(angle) * (px - ox) + math.cos(angle) * (py - oy)
-    return qx, qy
-
-def check_goal_reached(init_pose, goal_pose):
-    """
-        Check if goal is reached
-
-        Args:
-
-        init_pose: position of the turtlebot
-
-        goal_pose: goal position
-
-        Returns:
-
-        True if init_pose close to the goal_pose, otherwise false
-        """
-    if(init_pose.pose.position.x > goal_pose.pose.position.x - 0.1 and init_pose.pose.position.x < goal_pose.pose.position.x + 0.1\
-        and init_pose.pose.position.y > goal_pose.pose.position.y - 0.1 and init_pose.pose.position.y < goal_pose.pose.position.y + 0.1):
+# def check_goal_reached(init_pose, goal_pose, bias):
+#     if(init_pose.pose.position.x > goal_pose.pose.position.x - bias and init_pose.pose.position.x < goal_pose.pose.position.x + bias\
+#         and init_pose.pose.position.y > goal_pose.pose.position.y - bias and init_pose.pose.position.y < goal_pose.pose.position.y + bias):
+#         return True
+#     else:
+#         return False
+    
+def check_goal_reached(init_pose, goal_pose, bias):
+    if(init_pose.pose.position.x > goal_pose.pose.position.x - bias and init_pose.pose.position.x < goal_pose.pose.position.x + bias\
+        and init_pose.pose.position.y > goal_pose.pose.position.y - bias and init_pose.pose.position.y < goal_pose.pose.position.y + bias):
         return True
     else:
         return False
 
-#Get initial position of turtlebot
-init_msg = rospy.wait_for_message('/id100/aruco_single/pose', PoseStamped)
+# def goal_callback(data):
+#     print("goal_callback started")
+    
+# init = rospy.Subscriber("/id100/aruco_single/pose", PoseStamped, goal_callback)
+# init_msg = rospy.wait_for_message('/id100/aruco_single/pose', PoseStamped)
+cmd_pub = rospy.Publisher('cmd_vel', Twist, queue_size=1)
+init_pose = rospy.wait_for_message('/id100/aruco_single/pose', PoseStamped)
+goal_pose = rospy.wait_for_message('/id101/aruco_single/pose', PoseStamped)
+twist = Twist()
+while not check_goal_reached(init_pose, goal_pose, 0.05):
+    init_pose = rospy.wait_for_message('/id100/aruco_single/pose', PoseStamped)
+    # goal_pose = rospy.wait_for_message('/id101/aruco_single/pose', PoseStamped)
 
-#Initialize navigation stack from actionlib
-nav_client = actionlib.SimpleActionClient('move_base', MoveBaseAction)
-nav_client.wait_for_server()
-goal = MoveBaseGoal()
-goal.target_pose.header.frame_id = "map"
-goal.target_pose.header.stamp = rospy.Time.now()
+    orientation_q = init_pose.pose.orientation
+    orientation_list = [orientation_q.x, orientation_q.y, orientation_q.z, orientation_q.w]
+    (roll, pitch, yaw) = euler_from_quaternion(orientation_list)
+    Orientation = yaw
+    dx = goal_pose.pose.position.x - init_pose.pose.position.x
+    dy = goal_pose.pose.position.y - init_pose.pose.position.y
+    distance = math.dist([init_pose.pose.position.x, init_pose.pose.position.y], [goal_pose.pose.position.x, goal_pose.pose.position.y])
+    goal_direct = math.atan2(dy, dx)
 
-# Since turtlebot has odemetry of x = 0 and y = 0 when booting up, directly get relative goal position from turtlebot
-# and multiply by a scale factor
-x = (goal_pose.pose.position.x - init_msg.pose.position.x)*1.65
-y = -(goal_pose.pose.position.y - init_msg.pose.position.y)*1.2
+    print("init_pose", [init_pose.pose.position.x, init_pose.pose.position.y])
+    print("goal_pose", [goal_pose.pose.position.x, goal_pose.pose.position.y])
+    print("Orientation", Orientation)
 
-#Get euler angle between the turtlebot and goal position
-orientation_q = init_msg.pose.orientation
-orientation_list = [orientation_q.x, orientation_q.y, orientation_q.z, orientation_q.w]
-(roll, pitch, yaw) = euler_from_quaternion (orientation_list)
+    print("goal_direct", goal_direct)
+    if(Orientation < 0):
+        Orientation = Orientation + 2 * math.pi
+    if(goal_direct < 0):
+        goal_direct = goal_direct + 2 * math.pi
 
-# Since turtlebot has orientation 0 after bringup, rotate the goal position in the camera image to fit turtlebot orientation.
-dx = goal_pose.pose.position.x - init_msg.pose.position.x
-dy = -(goal_pose.pose.position.y - init_msg.pose.position.y)
-theta = math.atan2(dy, dx)
-print("theta:",theta * 180 / math.pi)
-print("yaw: ", yaw * 180 / math.pi)
-x_after, y_after = rotate((0,0), (x,y), yaw)
+    theta = goal_direct - Orientation
 
-# Set up correct relative goal position.
-goal.target_pose.pose.position.x = x_after
-goal.target_pose.pose.position.y = y_after
-print(goal.target_pose.pose.position.x)
-print(goal.target_pose.pose.position.y)
+    if theta < 0 and abs(theta) > abs(theta + 2 * math.pi):
+            theta = theta + 2 * math.pi
+    elif theta > 0 and abs(theta - 2 * math.pi) < theta:
+        theta = theta - 2 * math.pi
+    
+    print("theta:", theta)
 
-goal.target_pose.pose.position.z = 0.0
-goal.target_pose.pose.orientation.x = 0.0
-goal.target_pose.pose.orientation.y = 0.0
-goal.target_pose.pose.orientation.z = goal_pose.pose.orientation.y
-goal.target_pose.pose.orientation.w = 0.0
-init_msg = rospy.wait_for_message('/id100/aruco_single/pose', PoseStamped)
-
-# Activate navigation stack with goal position for tuetlbot to navigate.
-nav_client.send_goal(goal, done_cb, active_cb, feedback_cb)
-finished = nav_client.wait_for_result()
-
-
-if not finished:
-    rospy.logerr("Action server not available")
-else:
-    rospy.loginfo(nav_client.get_result())
-
+    k2 = 2
+    linear = 0.5
+    angular = k2 * theta
+    twist.linear.x = linear * distance * math.cos(theta)
+    twist.angular.z = -angular
+    cmd_pub.publish(twist)
